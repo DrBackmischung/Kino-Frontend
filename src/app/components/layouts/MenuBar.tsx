cf0ebc5a68990869cf183f1f42aab393ee3d38f0
ORTECADS\annaK <anna.khristolyubova@gmail.com>
2021-12-19 12:38:11 +0100

    Search function (works) Select (not finished, not working yet)

--- src/app/components/layouts/MenuBar.tsx
+++ src/app/components/layouts/MenuBar.tsx
@@ -13,101 +13,101 @@ import ComingSoon from "../../pages/ComingSoon";
 import DetailsPage from "../../pages/DetailsPage";
 
 function MenuBar() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  const [pages, setPages] = useState([
-    { name: "Programm", link: "/programPage" },
-    { name: "Events", link: "/eventsPage" },
-    { name: "Preisübersicht", link: "/pricesOverviewPage" },
-    { name: "News", link: "/newsPage" },
-  ]);
+    const [pages, setPages] = useState([
+        { name: "Programm", link: "/programPage" },
+        { name: "Events", link: "/eventsPage" },
+        { name: "Preisübersicht", link: "/pricesOverviewPage" },
+        { name: "News", link: "/newsPage" },
+    ]);
 
-  return (
-    <BrowserRouter>
-      <AppBar position="fixed">
-        <Container fixed>
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              component={Link}
-              to={"/"}
-            >
-              <HomeIcon />
-            </IconButton>
-            <Box
-              sx={{
-                justifyContent: "space-evenly",
-                flexGrow: 1,
-                display: { xs: "flex" },
-              }}
-            >
-              {pages.map((page) => (
-                <Button
-                  key={page.name}
-                  //onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                  variant="contained"
-                  component={Link}
-                  to={page.link}
-                >
-                  {page.name}
-                </Button>
-              ))}
-            </Box>
-            <Box>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Routes>
-        <Route path="/" element={<ComingSoon />} />
-        <Route path="/programPage" element={<ProgrammPage />} />
-        <Route path="/eventsPage" element={<ComingSoon />} />
-        <Route path="/pricesOverviewPage" element={<ComingSoon />} />
-        <Route path="/newsPage" element={<ComingSoon />} />
-        <Route path="/DetailsPage" element={<DetailsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppBar position="fixed">
+                <Container fixed>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            component={Link}
+                            to={"/"}
+                        >
+                            <HomeIcon />
+                        </IconButton>
+                        <Box
+                            sx={{
+                                justifyContent: "space-evenly",
+                                flexGrow: 1,
+                                display: { xs: "flex" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <Button
+                                    key={page.name}
+                                    //onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: "white", display: "block" }}
+                                    variant="contained"
+                                    component={Link}
+                                    to={page.link}
+                                >
+                                    {page.name}
+                                </Button>
+                            ))}
+                        </Box>
+                        <Box>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Routes>
+                <Route path="/" element={<ComingSoon />} />
+                <Route path="/programPage" element={<ProgrammPage />} />
+                <Route path="/eventsPage" element={<ComingSoon />} />
+                <Route path="/pricesOverviewPage" element={<ComingSoon />} />
+                <Route path="/newsPage" element={<ComingSoon />} />
+                <Route path="/DetailsPage" element={<DetailsPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default MenuBar;
