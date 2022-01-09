f38a3043509cfb6f58021882254b05462ed86f88
Babett Müller <pepsix3@web.de>
2022-01-09 15:41:40 +0100

    Ratings + Comments

---
+++ src/app/components/Reviews.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Reviews(props: any){
+    const { comments } = props;
+
+    return(
+        <div>
+            <h3>Reviews:</h3>
+        </div>
+    )
+}
+
+export default Reviews;
--- src/app/pages/DetailsPage.tsx
+++ src/app/pages/DetailsPage.tsx
@@ -1,115 +1,119 @@
 /* eslint-disable */
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import MovieDetails from "../components/MovieDetails";
 import ShowPicker from "../components/ShowPicker";
 import {
-  CardMedia,
-  CircularProgress,
-  Container,
-  Grid,
-  ThemeProvider,
+    Box,
+    CardMedia,
+    CircularProgress,
+    Container,
+    Grid,
+    ThemeProvider,
 } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { useQuery } from "react-query";
+import {createTheme} from "@mui/material/styles";
+import {useQuery} from "react-query";
 import palette from "../config/Colours";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import IconButton from "@mui/material/IconButton";
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import ManageCheckout from "../components/ManageCheckout";
 import ErrorPage from "./ErrorPage";
 import LoadingAnimation from "../components/layouts/LoadingAnimation";
 import APIUrl from "../config/APIUrl";
 import Ratings from "../components/Ratings";
+import Reviews from "../components/Reviews";
 
 function DetailsPage(props: any) {
-  const [movieId, setMovieId] = useState();
-  const [openSeatBooking, setOpenSeatBooking] = useState(0);
-  const [selectedShow, setSelectedShow] = useState();
-  const { state }: any = useLocation();
-  let navigate = useNavigate();
-  const apiUrlAll = `${APIUrl.apiUrl}/movie/${movieId}`;
-  const { isLoading, data, refetch, error } = useQuery(
-    "movie",
-    () => fetch(apiUrlAll).then((res) => res.json()),
-    {
-      refetchOnWindowFocus: false,
-      enabled: false,
+    const [movieId, setMovieId] = useState();
+    const [openSeatBooking, setOpenSeatBooking] = useState(0);
+    const [selectedShow, setSelectedShow] = useState();
+    const {state}: any = useLocation();
+    let navigate = useNavigate();
+    const apiUrlAll = `${APIUrl.apiUrl}/movie/${movieId}`;
+    const {isLoading, data, refetch, error} = useQuery(
+        "movie",
+        () => fetch(apiUrlAll).then((res) => res.json()),
+        {
+            refetchOnWindowFocus: false,
+            enabled: false,
+        }
+    );
+    useEffect(() => {
+        setMovieId(state.movieId);
+    }, [state?.movieId]);
+    useEffect(() => {
+        if (movieId) {
+            refetch();
+        }
+    }, [movieId]);
+
+    if (isLoading) {
+        return <LoadingAnimation/>;
     }
-  );
-  useEffect(() => {
-    setMovieId(state.movieId);
-  }, [state?.movieId]);
-  useEffect(() => {
-    if (movieId) {
-      refetch();
+
+    if (error) {
+        return <ErrorPage/>;
     }
-  }, [movieId]);
 
-  if (isLoading) {
-    return <LoadingAnimation />;
-  }
+    const theme = createTheme(palette);
 
-  if (error) {
-    return <ErrorPage />;
-  }
+    const goBack = () => {
+        navigate(-1);
+    };
 
-  const theme = createTheme(palette);
-
-  const goBack = () => {
-    navigate(-1);
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container className="wholeContainer">
-        <IconButton onClick={goBack}>
-          <ArrowBackIosIcon />
-        </IconButton>
-        <Container className="movieContainer">
-          <Grid
-            sx={{
-              display: "grid",
-              gap: 5,
-              gridTemplateColumns: "repeat(2, 1fr)",
-            }}
-          >
-            <div className="imageContainer">
-              <CardMedia
-                component="img"
-                sx={{
-                  pt: "6%",
-                }}
-                src={data?.pictureLink}
-                alt="poster"
-              />
-            </div>
-            <Grid
-              className="detailsContainer"
-              sx={{
-                display: "grid",
-                gridTemplateRows: "repeat(2,1fr)",
-              }}
-            >
-              <MovieDetails selectedMovie={data} />
-              {/*TODO Add Ratings to Movies in Backend*/}
-              <Ratings value={2.5}/>
-              <ShowPicker
-                setOpenSeatBooking={setOpenSeatBooking}
-                movieId={movieId}
-                setSelectedShow={setSelectedShow}
-                data={data}
-              />
-            </Grid>
-            <br />
-          </Grid>
-        </Container>
-        {/*TODO Add Reviews to Movies in Backend*/}
-        <Reviews comments={comments}/>
-        <ManageCheckout show={selectedShow} open={openSeatBooking} />
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Container className="wholeContainer">
+                <IconButton onClick={goBack}>
+                    <ArrowBackIosIcon/>
+                </IconButton>
+                <Container className="movieContainer">
+                    <Grid
+                        sx={{
+                            display: "grid",
+                            gap: 5,
+                            gridTemplateColumns: "repeat(2, 1fr)",
+                        }}
+                    >
+                        <div className="imageContainer">
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    pt: "6%",
+                                }}
+                                src={data?.pictureLink}
+                                alt="poster"
+                            />
+                        </div>
+                        <Grid
+                            className="detailsContainer"
+                            sx={{
+                                display: "grid",
+                                gridTemplateRows: "repeat(2,1fr)",
+                            }}
+                        >
+                            <Box>
+                                <MovieDetails selectedMovie={data}/>
+                                {/*TODO Add Ratings to Movies in Backend*/}
+                                <Ratings ratingValue={2}/>
+                                <ShowPicker
+                                    setOpenSeatBooking={setOpenSeatBooking}
+                                    movieId={movieId}
+                                    setSelectedShow={setSelectedShow}
+                                    data={data}
+                                />
+                            </Box>
+                        </Grid>
+                        <br/>
+                    </Grid>
+                </Container>
+                {/*TODO Add Reviews to Movies in Backend*/}
+                <Reviews /*comments={comments}*//>
+                <ManageCheckout show={selectedShow} open={openSeatBooking}/>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default DetailsPage;
