cbbdb5435fe8597eb2a8dc155c1e317e04cc9af6
Babett Müller <pepsix3@web.de>
2022-01-19 09:57:38 +0100

    Cookies Notification

--- src/app/components/CookiesNotification.tsx
+++ src/app/components/CookiesNotification.tsx
@@ -10,7 +10,7 @@ function CookiesNotification(){
                 buttonText="Accept"
                 cookieName="gerneralCookies"
                 style={{ background: "#2B373B" }}
-                buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+                buttonStyle={{ color: "#4e503b", fontSize: "15px" }}
                 onAccept={(acceptedByScrolling) => {
                     if (acceptedByScrolling) {
                         alert("Cookies were accepted by the scrolling of the user!");
@@ -20,7 +20,7 @@ function CookiesNotification(){
                 }}
             >
                 This website uses functional cookies to enhance the user experience.{" "}
-                <span style={{ fontSize: "10px" }}>By using this website you agree to the terms of the usage of
+                <span style={{ fontSize: "12px" }}>By using this website you agree to the terms of the usage of
                 functional cookies!</span>
             </CookieConsent>
         </div>
