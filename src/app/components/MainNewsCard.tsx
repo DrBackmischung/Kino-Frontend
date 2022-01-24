d7b86aa0a078526d29d26d6e5d90cc068330620e
ORTECADS\annaK <anna.khristolyubova@gmail.com>
2022-01-24 19:52:11 +0100

    KINO-240
    Events & News Page
    Caused changes in the MenuBar
    Fehler behoben

--- src/app/components/MainNewsCard.tsx
+++ src/app/components/MainNewsCard.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 
 
