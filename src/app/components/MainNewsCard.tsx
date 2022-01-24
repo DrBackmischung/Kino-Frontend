a901c0abc91f9ecfe297fbf71e38874cc73bdc6d
ORTECADS\annaK <anna.khristolyubova@gmail.com>
2022-01-24 19:40:20 +0100

    KINO-240
    Events & News Page
    Caused changes in the MenuBar

---
+++ src/app/components/MainNewsCard.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+
+
+
+export default function MainNewsCard(props: any) {
+    const { data } = props;
+
+
+    const mainNews = data[0];
+
+
+
+    return (
+        <Paper
+            sx={{
+                position: 'relative',
+                backgroundColor: 'grey.800',
+                color: '#fff',
+                mb: 4,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: `url("https://source.unsplash.com/random")`,
+            }}
+        >
+            {/* Increase the priority of the hero background image */}
+            {<img style={{ display: 'none' }} src="https://source.unsplash.com/random" alt={mainNews.imageText} />}
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0,0,0,.3)',
+                }}
+            />
+            <Grid container>
+                <Grid item md={6}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            p: { xs: 3, md: 6 },
+                            pr: { md: 0 },
+                        }}
+                    >
+                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                            {mainNews.header}
+                        </Typography>
+                        <Typography variant="h5" color="inherit" align="justify">
+                            {mainNews.content}
+                        </Typography>
+
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
---
+++ src/app/pages/NewsAndEventsPage.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import APIUrl from "../config/APIUrl";
+import {useQuery} from "react-query";
+import {useState} from "react";
+import ErrorPage from "./ErrorPage";
+import LoadingAnimation from "../components/layouts/LoadingAnimation";
+import Toolbar from "../components/Toolbar";
+import MovieCard from "../components/MovieCard";
+import SearchField from "../components/SearchField";
+import MainNewsCard from "../components/MainNewsCard";
+import NewsCard from "../components/NewsCard";
+
+
+
+
+const theme = createTheme();
+
+function NewsAndEventsPage() {
+
+    const apiUrlNews = `${APIUrl.apiUrl}/news/getAll`;
+
+    const newsData = useQuery("News", () =>
+        fetch(apiUrlNews).then((res) => res.json())
+    );
+
+
+    const [filter, setFilter] = useState("");
+
+    const handleSearchChange = (e: any) => {
+        setFilter(e.target.value.toLowerCase());
+    };
+
+    let prepareData: any = (dataItem: any) => {
+        if (newsData.data === undefined) return;
+        const sortedData = newsData.data?.sort((itemA: any, itemB: any) => {
+            if (itemA.date !== itemB.date) {
+                return (
+                    new Date(itemB.date).getTime() -
+                    new Date(itemA.date).getTime()
+                );
+            } else {
+                return (
+                    new Date(itemB.date).getTime() -
+                    new Date(itemA.date).getTime()
+                );
+            }
+        });
+        return sortedData;
+    };
+
+
+    if (newsData.error) {
+        return (
+            <Container
+                sx={{
+                    bgcolor: "background.paper",
+                    pt: 8,
+                    pb: 6,
+                    position: "relative",
+                    marginTop: "15rem",
+                }}
+                maxWidth="md"
+            >
+                <ErrorPage />
+            </Container>
+        );
+    }
+    if (newsData.isLoading)
+        return (
+            <Container
+                sx={{
+                    bgcolor: "background.paper",
+                    pt: 8,
+                    pb: 6,
+                    position: "relative",
+                    marginTop: "15rem",
+                }}
+                maxWidth="md"
+            >
+                <LoadingAnimation />
+            </Container>
+        );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container maxWidth="lg"
+                       sx={{marginTop: "7rem"}} >
+
+                <main>
+                    <MainNewsCard data={prepareData(newsData.data)}/>
+                    <SearchField handleSearchChange={handleSearchChange}/>
+                    <NewsCard  filter={filter}
+                               data={prepareData(newsData.data)}/>
+                </main>
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default NewsAndEventsPage;
