596f86646cf7f049202ca2365b095d79440eaf21
Mildem122 <77717082+Mildem122@users.noreply.github.com>
2022-01-28 11:45:40 +0100

    KINO-335B (#73)
    * KINO-335B
    
    * Update CheckoutDialog.tsx
    
    Co-authored-by: I538952 <marcel.mildenberger@sap.com>

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -1,36 +1,48 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
-import {CardMedia} from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import { CardMedia } from "@mui/material";
 import ReactPlayer from "react-player";
 import "./Slider.css";
 
-
 function Slider(props: any) {
-    const { selectedMovie } = props;
+  const { selectedMovie } = props;
 
-    const customRenderItem = (item: any, props: any) => <item.type {...item.props} {...props} />;
+  const customRenderItem = (item: any, props: any) => (
+    <item.type {...item.props} {...props} />
+  );
 
-    const YoutubeSlide = ({ url, isSelected }: { url: string; isSelected?: boolean }) => (
-        <ReactPlayer width="100%" url={url} playing={isSelected} style={{paddingTop: "20%"}}/>
-    );
+  const YoutubeSlide = ({
+    url,
+    isSelected,
+  }: {
+    url: string;
+    isSelected?: boolean;
+  }) => (
+    <ReactPlayer
+      width="100%"
+      url={url}
+      playing={isSelected}
+      style={{ paddingTop: "20%" }}
+    />
+  );
 
-    return (
-        <div className="carouselContainer">
-            <Carousel className="carousel"  renderItem={customRenderItem}>
-                <div>
-                    <CardMedia
-                        component="img"
-                        sx={{
-                           pl: "15%",
-                        }}
-                        src={selectedMovie?.pictureLink}
-                        alt="poster"
-                    />
-                </div>
-                <YoutubeSlide key="1" url={selectedMovie?.trailerLink}/>
-            </Carousel>
+  return (
+    <div className="carouselContainer">
+      <Carousel className="carousel" renderItem={customRenderItem}>
+        <div>
+          <CardMedia
+            component="img"
+            sx={{
+              pl: "15%",
+            }}
+            src={selectedMovie?.pictureLink}
+            alt="poster"
+          />
         </div>
-      );
+        <YoutubeSlide key="1" url={selectedMovie?.trailerLink} />
+      </Carousel>
+    </div>
+  );
 }
 
 export default Slider;
